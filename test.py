024fc713d898e42078d06b86a24963bad865a524
wanzare <wanzare@coli.uni-saarland.de>
2019-03-20 09:37:22 +0100

    update

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import unittest
 import spellChecker.read as read
 class Test(unittest.TestCase):
-
+    from urllib import request
+    url = "http://www.gutenberg.org/files/2554/2554.txt"
+    response = request.urlopen(url)
+    raw = response.read().decode('utf8')
+    print(raw)
     i2 = ["power", "trusted"]
     FILE = "test_corpus.txt"
     vocab = read.create_vocab(FILE)
     checker = read.Checker(vocab)
     words = ["powr", "trustedd"]
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-    def test_vocab(self):
 
+    def test_vocab(self):
         self.assertEqual(len(Test.vocab), 93)
 
     def test_checker(self):
